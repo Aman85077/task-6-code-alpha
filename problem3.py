9e17614e068904f61e3c0ce725290b858b99832e
Aman85077 <147108099+Aman85077@users.noreply.github.com>
2024-01-10 22:56:03 +0530

    problem3.py

---
+++ problem3.py
@@ -0,0 +1,13 @@
+a = [5, 3, 7, 4, 2]
+print("Maximum is:",max(a))
+print("Minimum is:",min(a))
+
+
+print()
+for i in a:
+    if i > a[0]:
+        MAXIMUM = i
+    elif i < a[0]:
+        MINIMUM = i
+print("Maximum is:", MAXIMUM)
+print("Minimum is:", MINIMUM)
